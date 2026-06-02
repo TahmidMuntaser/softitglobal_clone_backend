7ba2875677ab5cdc636ba71810f2f83436681334
Tahmid <tahmid25muntaser@gmail.com>
2026-06-03 00:22:21 +0600

    UPDATE: root endpoint to redirect to API docs

--- config/urls/__init__.py
+++ config/urls/__init__.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+def home(request):
+    return redirect('/api/docs/')
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/openapi/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
